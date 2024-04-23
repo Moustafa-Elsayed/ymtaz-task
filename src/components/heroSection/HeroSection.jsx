9586818f11c975eb13382e25d879cafc76f2f5be
moustafa elsayed <moelsayed949@gmail.com>
2024-04-23 21:25:16 +0200

    add

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -6,7 +6,6 @@ function HeroSection() {
   return (
     <>
       <div className="flex justify-center items-center ">
-        <CustomHeroSection title="الدليل الرقمي" />
       </div>
       <div className="p-3 grid grid-cols-3 gap-4 px-32">
         <CustomCard src={Logo} title="موثق" header="متوفر عدد 17" />
